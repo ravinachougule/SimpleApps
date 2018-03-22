6fdca71956966e962bc530b8829241c559c5d7cb
ravinachougule <ravinachougule7@gmail.com>
2018-03-22 11:23:20 +0530

    context and view demo

--- app/src/main/java/com/example/lenovo/simpleapps/MainActivity.kt
+++ app/src/main/java/com/example/lenovo/simpleapps/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.lenovo.simpleapps
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,20 +13,76 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnOK.setOnClickListener{
 
-            val no=etNum.text.toString()
-            val pass=etPass.text.toString()
-            if(no==pass)
-            {
-                txtResult.text="Successful"
-                Toast.makeText(this,"Login Successful",Toast.LENGTH_LONG).show()
+        val cntx: Context = this@MainActivity
+
+        // btnOK.setOnClickListener(this::clicked)
+
+        /*
+
+        val clk:View.OnClickListener=object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val no = etNum.text.toString()
+                val pass = etPass.text.toString()
+                if (no == pass) {
+                    txtResult.text = "Successful"
+                   // Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+                } else {
+                    txtResult.text = "Fail"
+                    //Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_LONG).show()
+                }
             }
-            else
-            {
-                txtResult.text="Fail"
-                Toast.makeText(this,"Login Unsuccessful",Toast.LENGTH_LONG).show()
+
+        }
+
+        btnOK.setOnClickListener(clk)
+        */
+
+
+
+        val clk2:View.OnClickListener= View.OnClickListener {
+            val no = etNum.text.toString()
+            val pass = etPass.text.toString()
+            if (no == pass) {
+                txtResult.text = "Successful"
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+            } else {
+                txtResult.text = "Fail"
+                Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_LONG).show()
             }
         }
+        btnOK.setOnClickListener(clk2)
+
+
+
+       /* btnOK.setOnClickListener {
+
+            val no = etNum.text.toString()
+            val pass = etPass.text.toString()
+            if (no == pass) {
+                txtResult.text = "Successful"
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+            } else {
+                txtResult.text = "Fail"
+                Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_LONG).show()
+            }
+        }
+        val vw: View = btnOK*/
+
+        val lyt:LinearLayout=LinearLayout(this@MainActivity)
     }
+
+
+    /*private fun clicked(vw:View)
+    {
+        val no = etNum.text.toString()
+        val pass = etPass.text.toString()
+        if (no == pass) {
+            txtResult.text = "Successful"
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+        } else {
+            txtResult.text = "Fail"
+            Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_LONG).show()
+        }
+    }*/
 }
